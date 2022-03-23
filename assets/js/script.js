cabe480d1e0963a3afb12e2123019983ed1472da
petehodnefield <98546095+petehodnefield@users.noreply.github.com>
2022-03-23 10:04:32 -0500

    appending tasks and type into the list

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,10 +5,34 @@ var createTaskHandler = function() {
     
     event.preventDefault();
 
-    var taskItemEl = document.createElement("li");
-    taskItemEl.className = "task-item";
-    taskItemEl.textContent = "This is a new task.";
-    tasksToDoEl.appendChild(taskItemEl);
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+
+    // create list item
+    var listItemEl = document.createElement("li");
+    listItemEl.className = "task-item";
+
+    // create div to hold task info and add to list item
+    var taskInfoEl = document.createElement("div");
+    
+    // give it a class name
+    taskInfoEl.className = "task-info";
+
+    // add HTML content to div
+    taskInfoEl.innerHTML = 
+    "<h3 class='task-name'>" + 
+    taskNameInput + 
+    "</h3><span class='task-type'>" + 
+    taskTypeInput + 
+    "</span>";
+
+    listItemEl.appendChild(taskInfoEl);
+
+    // add entire item to list
+    tasksToDoEl.appendChild(listItemEl);
+
+    console.dir(listItemEl);
+
 }
 
 formEl.addEventListener("submit", createTaskHandler);
